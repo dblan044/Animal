46c0ca1a578d518fa1e0526893b0d30cdbb23c9b
Diana R Blanco <54902184+dblan044@users.noreply.github.com>
2021-05-20 15:02:28 -0400

    Update Main.java
    added print() method from respective classes for a cleaner main.

--- Main.java
+++ Main.java
@@ -3,11 +3,7 @@ public class Main {
         //dog object
         Dog pet1 = new Dog("Alucard", "Doberman", "Black", 10, "T-Bone", 
                                     false, true);
-        
-        System.out.println(pet1.getName() + " is a " + pet1.getTypeOfAnimal()
-                            + ", his coat is " + pet1.getColor()+ ", and he is " + 
-                            pet1.getAge()+ " years old. " + "His favorite food is " 
-                            + pet1.getFavFood() + ".");
+        pet1.print();
         pet1.isBarking(true);
         pet1.isPlaying(true);
         
@@ -16,10 +12,7 @@ public class Main {
                                 false, true);
         System.out.println("");
         
-        System.out.println(pet2.getName() + " is a " + pet2.getTypeOfAnimal()
-                            + ", her coat is " + pet2.getColor()+ ", and she is " + 
-                            pet2.getAge()+ " years old. " + "Her favorite food is " 
-                            + pet2.getFavFood() + ".");
+        pet2.print();
         pet2.isMeowing(true);
         pet2.isPlaying(true);
     }

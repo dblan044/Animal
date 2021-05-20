b89738a373400657287f98290d1b141745b7aa98
Diana R Blanco <54902184+dblan044@users.noreply.github.com>
2021-05-20 13:01:36 -0400

    Removal
    removed unnecessary import statements.

--- Main.java
+++ Main.java
@@ -1,12 +1,3 @@
-import java.util.*;
-import java.util.Scanner;
-import java.math.BigDecimal;
-import java.text.NumberFormat;
-import java.util.Locale;
-import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Main { 
     public static void main(String[] args) {
         //dog object
